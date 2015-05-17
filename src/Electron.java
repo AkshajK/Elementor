52ae18515bf4887858017f0e2e70fa5fcc00bef8
justinz2018 <justin.z2013@hotmail.com>
2015-05-16 23:41:49 -0400

    Readded no-arg constructors lost in merge

--- src/Electron.java
+++ src/Electron.java
@@ -2,6 +2,9 @@ import java.awt.Color;
 
 
 public class Electron extends Subatomic{
+	public Electron(){
+		super(Color.RED, 50);
+	}
 	public Electron(int x, int y){
 		super(Color.RED, x, y, 20);
 	}
--- src/Proton.java
+++ src/Proton.java
@@ -2,6 +2,9 @@ import java.awt.Color;
 
 
 public class Proton extends Subatomic{
+	public Proton(){
+		super(Color.RED, 50);
+	}
 	public Proton(int x, int y){
 		super(Color.RED, x, y, 20);
 	}
